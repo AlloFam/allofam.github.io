d9b04c1edeabfbda25dcc7abe0041f692cfd8d8a
gizebonilla <gize.bonilla@gmail.com>
2025-02-13 23:29:43 -0800

    base service

---
+++ src/components/elements/Card.tsx
@@ -0,0 +1,19 @@
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+
+interface CardServiceProps {
+  title: string;
+  src: string;
+}
+
+const CardService = ({ title, src }: CardServiceProps) => (
+  <Card className="w-[300px] m-5">
+    <CardContent className="py-5 flex justify-center">
+      <img src={src} alt={title} className="w-[128px]" />
+    </CardContent>
+    <CardFooter className="flex justify-center bg-teal-s75 p-5 rounded-b-lg">
+      <p className="font-bold text-teal-s500">{title}</p>
+    </CardFooter>
+  </Card>
+);
+
+export default CardService;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,34 @@
+import CardService from "@/components/elements/Card";
 import heroImg from "../assets/hero.png";
+//  Icons
+import doctorImg from "../assets/icons/doctor.png";
+import chatImg from "../assets/icons/chat.png";
+import oralHealthImg from "../assets/icons/oral-health.png";
+import physiotherapistImg from "../assets/icons/physiotherapist.png";
+import speechTherapistImg from "../assets/icons/speech-therapist.png";
+
+const cards = [
+  {
+    title: "Physical Therapists",
+    src: physiotherapistImg,
+  },
+  {
+    title: "Pediatric Dentists ",
+    src: oralHealthImg,
+  },
+  {
+    title: "Occupational Therapists",
+    src: speechTherapistImg,
+  },
+  {
+    title: "Speech Therapist",
+    src: chatImg,
+  },
+  {
+    title: "Pediatricians",
+    src: doctorImg,
+  },
+];
 
 const Home = () => {
   return (
@@ -12,23 +42,47 @@ const Home = () => {
             healthcare provider for <br />
             your young child
           </h1>
-          <h2 className="mt-5 text-lg font-bold">
+          <p className="mt-5 text-2xl">
             Consider us your “friend with the best recommendations” across
             healthcare providers for your child.
-          </h2>
+          </p>
         </section>
         <div className="py-10 w-[560px] z-1">
           <img src={heroImg} alt="hero" className="w-full" />
         </div>
         <div className="absolute bottom-0 right-0 w-200 h-100 bg-white clip-triangle-br"></div>
       </div>
-      <section className="bg-light-blue flex flex-col items-center p-10">
+      <section className="bg-teal-s50 flex flex-col items-center p-20">
         <h2 className="text-5xl font-bold text-center text-teal">
           Your child needs healthcare providers.
         </h2>
         <h2 className="text-5xl font-bold text-center text-teal">
           We have a recommendation for you.
         </h2>
+        <hr className="w-[50%] bg-teal h-[2px] my-10" />
+        <p className="mx-40 text-2xl mb-5 text-center">
+          Currently serving families through our Concierge Services in Denver
+          and San Francisco, we search our network of resources to find exactly
+          what your family needs to support your child in their medical journey.
+        </p>
+        <p className="mx-40 text-2xl text-center">
+          Stay tuned for when we expand our Concierge Services to your city and
+          for when our self-service platform launches
+        </p>
+        <div className="flex justify-center m-10 flex-wrap">
+          {cards.map(({ title, src }, idx) => (
+            <CardService key={`card-${idx}`} title={title} src={src} />
+          ))}
+        </div>
+        <p className="mx-40 text-2xl my-5 text-center">
+          We are detailed in our approach to ensure that your values are
+          reflected in the providers sourced: house calls, office proximity,
+          ease of scheduling, sensory-sensitive office space and more.
+        </p>
+        <p className="mx-40 text-2xl mb-10 text-center">
+          Have you had it on your to-do list to find a provider for your child
+          and want support? Schedule a free intake call below:
+        </p>
       </section>
     </div>
   );
